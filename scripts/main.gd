87af1e45f1e0f5f1130dd34a1a17022401774f9d
kvnfrr <kevinferrercs@gmail.com>
2026-06-08 20:31:44 -0400

    Updated main script to work with spirit

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,13 +1,19 @@
 extends Node2D
 
-@onready var path_line: Line2D = $PathLine
+enum GameState { DRAWING, RUNNING }
 
+@onready var path_line: Line2D = $PathLine
+@onready var spirit: Area2D = $Spirit
+
+var state := GameState.DRAWING
 var path_points: Array[Vector2] = []
 var min_point_distance := 8.0
-var can_draw := true
+
+var spirit_index := 0
+var spirit_speed := 120.0
 
 func _unhandled_input(event: InputEvent) -> void:
-	if not can_draw:
+	if state != GameState.DRAWING:
 		return
 
 	if event is InputEventMouseButton:
@@ -21,6 +27,13 @@ func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("clear_path"):
 		clear_path()
 
+	if Input.is_action_just_pressed("start_run") and path_points.size() > 1:
+		start_run()
+
+func _process(delta: float) -> void:
+	if state == GameState.RUNNING:
+		move_spirit(delta)
+
 func add_path_point(pos: Vector2) -> void:
 	if path_points.is_empty() or path_points[-1].distance_to(pos) >= min_point_distance:
 		path_points.append(pos)
@@ -29,3 +42,19 @@ func add_path_point(pos: Vector2) -> void:
 func clear_path() -> void:
 	path_points.clear()
 	path_line.clear_points()
+
+func start_run() -> void:
+	state = GameState.RUNNING
+	spirit.global_position = path_points[0]
+	spirit_index = 1
+
+func move_spirit(delta: float) -> void:
+	if spirit_index >= path_points.size():
+		state = GameState.DRAWING
+		return
+
+	var target := path_points[spirit_index]
+	spirit.global_position = spirit.global_position.move_toward(target, spirit_speed * delta)
+
+	if spirit.global_position.distance_to(target) < 2.0:
+		spirit_index += 1
